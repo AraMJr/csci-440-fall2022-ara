2e5af04fd6af7a96923ca4b5a8cf87733914589d
Carson Gross <carson@bigsky.software>
2022-11-14 16:49:00 -0700

    fix test

--- src/test/java/edu/montana/csci/csci440/homework/Homework3.java
+++ src/test/java/edu/montana/csci/csci440/homework/Homework3.java
@@ -39,9 +39,9 @@ public class Homework3 extends DBTest {
             subtract.execute();
 
             PreparedStatement add = connection.prepareStatement("TODO");
-            subtract.setLong(1, 0);
-            subtract.setLong(2, 0);
-            subtract.execute();
+            add.setLong(1, 0);
+            add.setLong(2, 0);
+            add.execute();
 
             // commit with the connection
         }
